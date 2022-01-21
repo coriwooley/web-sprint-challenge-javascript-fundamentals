aa1641141decacbe2bfff0887a336b40f91ed0f4
Cori Wooley <wooleycori@gmail.com>
2022-01-21 04:31:04 -0800

    callbacks step 2

--- index.js
+++ index.js
@@ -125,8 +125,8 @@ const zooAnimals = [
   /* 🦁🦁🦁 Step 2: Create several functions to callback with consume(); 🦁🦁🦁 */
  // 🦁🦁🦁 Use add to return the sum of two numbers 🦁🦁🦁
   
-function add(/*Your Code Here */){
-    /*Your Code Here*/
+function add(num1, num2){
+    return num1 + num2;
   }
 
 
